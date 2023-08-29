ba50958dced095a6ab2a700d08087276a61e26a6
Zhuoxin <lizhuoxinmike@gmail.com>
2023-08-29 17:22:10 +0800

    finished up to tut6

--- models/author.js
+++ models/author.js
@@ -33,6 +33,11 @@ AuthorSchema.virtual("date_of_birth_formatted").get(function () {
 AuthorSchema.virtual("date_of_death_formatted").get(function () {
   return this.date_of_death? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
 });
+AuthorSchema.virtual("lifespan").get(function () {
+  birth = this.date_of_birth? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
+  death = this.date_of_death? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
+  return birth + "-" + death;
+});
 
 // Export model
 module.exports = mongoose.model("Author", AuthorSchema);
